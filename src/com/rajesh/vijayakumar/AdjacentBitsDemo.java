530f5636d85bfba662f1cd78c379862871b0dfe9
Rajesh Vj <rajeshvijayakumar@outlook.com>
2018-09-30 14:35:57 +0530

    Added: Core Java : Check if a number has two adjacent set bits

--- src/com/rajesh/vijayakumar/AdjacentBitsDemo.java
+++ src/com/rajesh/vijayakumar/AdjacentBitsDemo.java
@@ -2,8 +2,24 @@ package com.rajesh.vijayakumar;
 
 public class AdjacentBitsDemo {
 
-	public static void main(String[] args) {
-		
-		System.out.println("Welcome to Java");
-	}
+	static boolean adjacentSet(int n) { 
+        int x = (n & (n >> 1)); 
+          
+        if(x == 1) 
+            return true; 
+        else
+            return false; 
+    } 
+      
+    public static void main(String args[])  
+    { 
+ 
+        int n = 3; 
+          
+        if(adjacentSet(n)) 
+            System.out.println("Yes"); 
+        else
+            System.out.println("No");  
+  
+    } 
 }
